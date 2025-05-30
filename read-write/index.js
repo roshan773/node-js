9cc48f64881e83f0daf5572575b22e26cd64499d
Pakhre Roshan <pakhreroshan@gmail.com>
2025-05-30 13:30:31 +0530

    read data and write data in the db using read and write

--- read-write/index.js
+++ read-write/index.js
@@ -1,13 +1,29 @@
 const fs = require("fs")
 
-function writeMessagetofile(message){
-    fs.appendFile("./file.txt", `\n ${message}`, (err) => {
+let productdata = {
+    id: Math.random().toString(36).substr(2,9),
+    title: "title 2",
+    price: 200,
+}
+
+
+function Adddata(){
+    fs.readFile("./db.json", "utf-8", (err, data) => {
         if (err) {
             console.log(err)
         } else {
-           console.log("data written successfully.....") 
+            const parseData = JSON.parse(data)
+            parseData.push(productdata)
+
+            fs.writeFile("./db.json", JSON.stringify(parseData), (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Data Written Successfully......")                    
+                }
+            })
         }
     })
 }
 
-writeMessagetofile("BYE")
+Adddata()
